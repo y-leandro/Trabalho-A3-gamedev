bc053c1957680d8b7936b55b45ecb80e4d63f148
isadahmerb <idahmerbarbosa@gmail.com>
2025-05-20 21:05:50 -0300

    add menu1 e 2

---
+++ main_menu/main_menu.gd
@@ -0,0 +1,25 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$menu1/MarginContainer/HBoxContainer/VBoxContainer/ContinueButton.connect("button_down", continuar_game)
+	$menu1/MarginContainer/HBoxContainer/VBoxContainer/NewgameButton.connect("button_down", new_jogo)
+	$menu1/MarginContainer/HBoxContainer/VBoxContainer/OptionsButton.connect("button_down", opcoes)
+	$menu1/MarginContainer/HBoxContainer/VBoxContainer/QuitButton.connect("button_down", quit)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func continuar_game():
+	print("comococo")
+
+func new_jogo():
+	get_tree().change_scene_to_file("")
+	
+	# add cena real
+	
+func opcoes ():
+	print ("vanessaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+
+func quit():
+	get_tree().quit()
